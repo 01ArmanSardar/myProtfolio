162d0558d36e922df4c910ba9b73858eb80d25c3
ArmanSardar <mdarmansardar57@gmail.com>
2025-01-07 22:35:22 +0600

    style in navbar,skill and about

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -1,10 +1,36 @@
-
+import { AiFillApi } from "react-icons/ai";
+import { FaDatabase } from "react-icons/fa";
+import { RiComputerLine } from "react-icons/ri";
 
 const About = () => {
     return (
-        <div>
-            <h1>ABout of me</h1>
+        <div className="">
+            <div className=" p-6 text-center font-serif">
+                <p>Hi! This is ARMAN Currently, I have completed my Diploma in Electronics Engineering from Lakshmipur Polytechnic Institute. I have deep interest in Development and also I have completed CSE Fundamentals with Phitron</p>
+            </div>
+            <div className="p-5 bg-slate-400">
+                <h1 className="text-2xl">What would i offer</h1>
+                <div className="grid grid-cols-2 gap-2 text-center">
+                    <div className=" bg-base-200 rounded-xl flex p-7">
+                    <RiComputerLine className="text-5xl text-emerald-600" />
+                        <h1> Frontend Development
+                            I can build a beautiful and scalable SPA using HTML,CSS, React.js, Next.js and Jquery</h1>
+                    </div>
+                    <div className="bg-base-200 rounded-xl flex p-7">
+                    <FaDatabase className="text-3xl text-emerald-600" />
+                        <h1>Backend Development
+                            handle database, server, api using Express & other popular frameworks</h1>
+                    </div>
+                    <div className="bg-base-200 rounded-xl flex p-7">
+                    <AiFillApi className="text-3xl text-emerald-600"/>
+                        <h1>API Development
+                        I can develop robust REST API using Django</h1>
+                    </div>
+                </div>
+            </div>
+
         </div>
+
     );
 };
 
--- src/Component/NavBar.jsx
+++ src/Component/NavBar.jsx
@@ -1,15 +1,24 @@
 import { NavLink } from "react-router-dom";
-
+import { FaLinkedin } from "react-icons/fa6";
+import { FaSquareGithub } from "react-icons/fa6";
+import { FaFacebook } from "react-icons/fa6";
 import Myimage from '../../public/Picsart_23-08-27_20-55-51-979.jpg'
+import './NavBar.css'
 const NavBar = () => {
     return (
         <div className="text-center">
             <img className="rounded-full w-3/4 m-auto" src={Myimage} alt="" />
-            <NavLink to={'/'}>Home</NavLink> <br />
-            <NavLink to={'/about'}>about</NavLink> <br />
-            <NavLink to={'/blog'}>blog</NavLink> <br />
-            <NavLink to={'/project'}>project</NavLink> <br />
-            <NavLink to={'/skill'}>Skill</NavLink>
+            <h1  className=" name text-3xl "> <span className="text-green-500">Arman</span> Sardar</h1>
+            <div className="flex justify-evenly">
+            <FaLinkedin  className="text-teal-800 text-3xl"/>
+            <FaSquareGithub className="text-teal-800 text-2xl"/>
+            <FaFacebook className="text-teal-800 text-1xl"/>
+            </div>
+            {/* <NavLink className={' btn btn-outline btn-info m-1' }   to={'/'}>Home</NavLink> <br /> */}
+            <NavLink  className={' btn btn-outline btn-info m-1' }  to={'/about'}>about</NavLink> <br />
+            
+            <NavLink className={' btn btn-outline btn-info m-1' }  to={'/project'}>project</NavLink> <br />
+            <NavLink className={' btn btn-outline btn-info m-1' }  to={'/skill'}>Skill</NavLink>
         </div>
     );
 };
