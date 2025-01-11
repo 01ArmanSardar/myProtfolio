4e25beaa28d3eb2a1bbf19e244506723dab012c5
ArmanSardar <mdarmansardar57@gmail.com>
2025-01-11 21:54:38 +0600

    flash

--- src/Component/Project.jsx
+++ src/Component/Project.jsx
@@ -11,20 +11,20 @@ const Project = () => {
                     <input type="radio" name="my-accordion-1" defaultChecked />
                     <div className="collapse-title text-xl font-medium">description</div>
                     <div className="collapse-content">
-                        
-                            <li>Implimentend user-friendly registration with Password toggle visivility (show/hide)</li>
-                            <li>Ensured presistent login sessions using Firebase onAuthSTateChanged method</li>
-                            <li>added protected routes for Estate Details to restrict acces to logged-in users only</li>
-                            <li>Developed an Estates section showcasing categorized property
-                                <li className="mx-8">image ,title ,price ,status,(sale/rent),location,area and facilities</li>
-                                <li className="mx-8">A  viewProperty button to Navigate to detaild estate page</li>
-                            <li>included email-based and Google login authentication with password validation</li>    
+
+                        <li>Implimentend user-friendly registration with Password toggle visivility (show/hide)</li>
+                        <li>Ensured presistent login sessions using Firebase onAuthSTateChanged method</li>
+                        <li>added protected routes for Estate Details to restrict acces to logged-in users only</li>
+                        <li>Developed an Estates section showcasing categorized property
+                            <li className="mx-8">image ,title ,price ,status,(sale/rent),location,area and facilities</li>
+                            <li className="mx-8">A  viewProperty button to Navigate to detaild estate page</li>
+                            <li>included email-based and Google login authentication with password validation</li>
                             <li>Created a conditional Navbar showing user info when logged in or a logout button otherwise</li>
                             <li>Added Toast/SweetAlert notifications for succcesfully actions and error handeling</li>
                             <li>Developed a custom 404 page for non-existent routes to ensure seamless navigation</li>
 
-                            </li>
-                        
+                        </li>
+
                     </div>
                 </div>
                 <div className="collapse bg-base-200">
